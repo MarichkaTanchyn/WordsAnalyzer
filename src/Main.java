acadea1befaa03806a5f48bb8f72b27d6bd6bfef
Maria <mariatanshyn@gmail.com>
2021-10-26 20:50:49 +0200

    Reader

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> data = MyReader.read("data/text.txt");
+
+        for (Map.Entry<String, Integer> pair : data.entrySet()) {
+            System.out.println(pair);
+        }
+    }
+}
--- src/MyReader.java
+++ src/MyReader.java
@@ -1,2 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
 public class MyReader {
+    public static Map<String, Integer> read(String path) {
+        String row;
+        String[] rowData;
+        String[] words;
+        Map<String, Integer> data = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while ((row = reader.readLine()) != null) {
+                rowData = row.split("\\s+");
+                for (int i = 0; i < rowData.length; i++) {
+                    if (data.containsKey(rowData[i])) {
+                        int count = data.get(rowData[i]);
+                        count++;
+                        data.put(rowData[i], count);
+                    } else {
+                        data.put(rowData[i], 1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
 }
